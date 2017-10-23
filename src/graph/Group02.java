b76f46e81ddc6ab8380288ecbb80915bb099c33d
Liad Golan <liad.golan.736@my.csun.edu>
2017-10-22 17:32:05 -0700

    Starting commit, with all the setup

---
+++ src/graph/Group02.java
@@ -0,0 +1,10 @@
+package graph;
+
+public class Group02
+{
+    public static void main(String[] args)
+    {
+        Group02 mygraph = new Group02(args[0]);
+        mygraph.shortestPath(args[1].get(0));
+    }
+}
