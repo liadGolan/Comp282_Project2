71b78e3521dc5f3766a2edc2079357b5e3c049b2
Liad Golan <liad.golan.736@my.csun.edu>
2017-10-29 16:40:55 -0700

    Refactored double array

--- src/graph/Group02.java
+++ src/graph/Group02.java
@@ -2,12 +2,12 @@ package graph;
 
 public class Group02
 {
+    public int[][] graph = new int[10][10];
+
     public static void main(String[] args)
     {
         Group02 mygraph = new Group02(args[0]);
         mygraph.shortestPath(args[1].get(0));
-
-        int[][] graph = new int[10][10];
 
         this.storeData('a', 'o', 1);
         this.storeData('b', 'a', 7);
