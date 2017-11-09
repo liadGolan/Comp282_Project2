1cce881ecdd29e121d3fc2e900f02f34b5033db7
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2017-11-08 19:46:10 -0800

    added fileReader and BufferedReader

--- src/graph/Group02.java
+++ src/graph/Group02.java
@@ -1,51 +1,57 @@
 package graph;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
 public class Group02
 {
+//VARIABLES
+    private int locColA;
+    private int locColB;
+    private int distVal;
+    private String fileName;
+    private char[] storeArr;
     public int[][] graph = new int[10][10];
 
-    public static void main(String[] args)
-    {
-        Group02 mygraph = new Group02(args[0]);
-        mygraph.shortestPath(args[1].charAt(0));
+//CONSTRUCTOR
+    public Group02(String fileName){
+        try
+        {
+            this.fileName = fileName;
+            BufferedReader br = new BufferedReader(new FileReader(fileName)); //looks for file name sample.txt
+            String line = br.readLine(); // reads every line of the file as a string
 
-        this.storeData('a', 'o', 1);
-        this.storeData('b', 'a', 7);
-        this.storeData('b', 'c', 3);
-        this.storeData('c', 'b', 6);
-        this.storeData('d', 'r', 8);
-        this.storeData('e', 'a', 8);
-        this.storeData('e', 'b', 7);
-        this.storeData('e', 'h', 8);
-        this.storeData('f', 'i', 5);
-        this.storeData('f', 'j', 4);
-        this.storeData('g', 'j', 3);
-        this.storeData('g', 'k', 1);
-        this.storeData('h', 'a', 5);
-        this.storeData('h', 'e', 5);
-        this.storeData('h', 'i', 4);
-        this.storeData('h', 'o', 5);
-        this.storeData('j', 'f', 1);
-        this.storeData('j', 'k', 5);
-        this.storeData('j', 'm', 7);
-        this.storeData('k', 'n', 8);
-        this.storeData('k', 'r', 4);
-        this.storeData('l', 'h', 4);
-        this.storeData('l', 'p', 9);
-        this.storeData('m', 'n', 5);
-        this.storeData('o', 'p', 9);
-        this.storeData('p', 'm', 3);
-        this.storeData('p', 'r', 2);
-        this.storeData('r', 'k', 4);
+            while (line != null)
+            {
+                storeArr = line.toCharArray(); //store the line in the file into an array as chars
+                for (int i = 0; i < storeArr.length; i++) //for every index in the array of storeArr...
+                {
+                    locColA = ((int) storeArr[i])-97; //convert index i to int and store into locColA
+                    locColB = ((int) storeArr[i+1])-97;//convert index i+1 to int and store into locColB
+                    distVal = ((int) storeArr[i+2])-97;//convert index i+2 to int and store into distVal
+                    graph[locColA][locColB]= distVal;//store as a value in the 2D made up of locColA and locColB
+                }
+            }
+        }
+        catch(IOException exception)
+        {
+            System.out.println("Error processing file: " + exception);
+        }
+    }
+
+//MAIN
+    public static void main(String [] args) // supplied command-line arguments as an array of String objects
+    {
+        Group02 mygraph = new Group02(args[0]); // created a java object using classname
+        mygraph.shortestPath(args[1].charAt(0)); //calling the shortestPath class as an object
     }
 
     private void shortestPath(char c) {
+        //General Dijkstra algo
+
 
     }
 
-    public void storeData(char col1, char col2, int val){
-        int locColA = ((int) col1)-97;
-        int locColB = ((int) col2)-97;
-        graph [locColA][locColB] = val;
-    }
 }
